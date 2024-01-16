f93a78b6d2a3e8c9979952c27566913aa848ef02
Diego Delli Colli <dellicolli@gmail.com>
2024-01-16 09:41:17 -0300

    bug fix in useStarEvents hook

--- src/hook/useStarEvent.ts
+++ src/hook/useStarEvent.ts
@@ -5,22 +5,21 @@ import { useContext, useEffect, useRef } from "react";
 
 export default function useStarEvent(callback: () => any) {
   const ref = useRef<HTMLUListElement>(null);
-  const { starValue, setStarValue } = useContext(FormContext);
+  const { setStarValue } = useContext(FormContext);
 
   useEffect(() => {
     function outClick(event: MouseEvent) {
       const stars = document.querySelectorAll(".star-icon");
       const targetLi = event.target as HTMLLIElement;
       const classList = !targetLi?.classList.contains("active");
+      const tag = targetLi.tagName;
 
-      if (!ref.current !== classList) {
+      if (!ref.current !== classList && targetLi && tag === "LI") {
         stars.forEach(function (star) {
           star.classList.remove("active");
 
           callback();
         });
-
-        const tag = targetLi.tagName;
 
         if (targetLi && tag === "LI") {
           targetLi.classList.add("active");
